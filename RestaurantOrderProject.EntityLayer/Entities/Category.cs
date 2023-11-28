fa8c139ba134f83c1e10137b104cc5bde6d3f70d
seheerst <seherst9@gmail.com>
2023-11-28 22:39:07 +0300

    feat: entity layer and entities created

---
+++ RestaurantOrderProject.EntityLayer/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace RestaurantOrderProject.EntityLayer.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public bool Status { get; set; }
+
+    }
+}
