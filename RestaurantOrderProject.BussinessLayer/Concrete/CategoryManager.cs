e3c21a7ab0a78f2623cebd524a97c6501eb59df6
seheerst <seherst9@gmail.com>
2023-11-30 21:53:48 +0300

    feat: created service manager for entities

---
+++ RestaurantOrderProject.BussinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using RestaurantOrderProject.BussinessLayer.Abstract;
+using RestaurantOrderProject.DataAccessLayer.Abstract;
+using RestaurantOrderProject.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOrderProject.BussinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private readonly ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public void TAdd(Category entity)
+        {
+            _categoryDal.Add(entity);
+        }
+
+        public void TDelete(Category entity)
+        {
+            _categoryDal.Delete(entity);
+        }
+
+        public List<Category> TGetAllList()
+        {
+           return _categoryDal.GetAllList();
+        }
+
+        public Category TGetById(int id)
+        {
+            return _categoryDal.GetById(id);
+        }
+
+        public void TUpdate(Category entity)
+        {
+            _categoryDal.Update(entity);
+        }
+    }
+}
---
+++ RestaurantOrderProject.BussinessLayer/Concrete/ProductManager.cs
@@ -0,0 +1,41 @@
+﻿using RestaurantOrderProject.BussinessLayer.Abstract;
+using RestaurantOrderProject.DataAccessLayer.Abstract;
+using RestaurantOrderProject.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOrderProject.BussinessLayer.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private readonly IProductDal _productDal;
+        public void TAdd(Product entity)
+        {
+            _productDal.Add(entity);
+        }
+
+        public void TDelete(Product entity)
+        {
+           _productDal.Delete(entity);
+        }
+
+        public List<Product> TGetAllList()
+        {
+            return _productDal.GetAllList();
+        }
+
+        public Product TGetById(int id)
+        {
+            return _productDal.GetById(id);
+        }
+
+        public void TUpdate(Product entity)
+        {
+            _productDal.Update(entity);
+           
+        }
+    }
+}
