a5bff379f0ce40f20d0681a5b7e591893df50143
seheerst <seherst9@gmail.com>
2023-12-02 23:51:40 +0300

    feat: dependency injection for aboutService and aboutDal initialize db context

--- RestaurantOrder_Api/Program.cs
+++ RestaurantOrder_Api/Program.cs
@@ -1,3 +1,9 @@
+using RestaurantOrderProject.BussinessLayer.Abstract;
+using RestaurantOrderProject.BussinessLayer.Concrete;
+using RestaurantOrderProject.DataAccessLayer.Abstract;
+using RestaurantOrderProject.DataAccessLayer.Concrete;
+using RestaurantOrderProject.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<RestaurantOrderContext>();
+
+builder.Services.AddScoped<IAboutService, AboutManager>();
+builder.Services.AddScoped<IAboutDal, EfAboutDal>();
 
 var app = builder.Build();
 
