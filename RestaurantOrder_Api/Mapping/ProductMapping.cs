27f969f3009b8d7b486786e38e4f370f37656c0c
seheerst <seherst9@gmail.com>
2023-12-02 23:47:15 +0300

    feat: generate mapping class for all entities

---
+++ RestaurantOrder_Api/Mapping/ProductMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using RestaurantOrderProject.DtoLayer.ProductDtos;
+using RestaurantOrderProject.EntityLayer.Entities;
+
+namespace RestaurantOrder_Api.Mapping
+{
+    public class ProductMapping:Profile
+    {
+        public ProductMapping() { 
+            CreateMap<Product, ResultProductDto>().ReverseMap();
+            CreateMap<Product, CreateProductDto>().ReverseMap();
+            CreateMap<Product, GetProductDto>().ReverseMap();
+            CreateMap<Product, UpdateProductDto>().ReverseMap();
+        }
+    }
+}
