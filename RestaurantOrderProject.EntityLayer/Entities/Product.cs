8203d0dc674b1978481fe6e57d34467462bb1b57
seheerst <seherst9@gmail.com>
2023-12-04 03:10:40 +0300

    feat: relation with category table

--- RestaurantOrderProject.EntityLayer/Entities/Product.cs
+++ RestaurantOrderProject.EntityLayer/Entities/Product.cs
@@ -8,8 +8,9 @@
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+
         public int CategoryID { get; set; }
         public Category Category { get; set; }
-        public bool Status { get; set; }
     }
 }
