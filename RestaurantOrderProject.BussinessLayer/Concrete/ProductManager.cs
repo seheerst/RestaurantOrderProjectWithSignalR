a65d978dfec1b144974ca46b433e62ec6195ca39
seheerst <seherst9@gmail.com>
2023-12-04 03:06:28 +0300

    feat: dependency injection

--- RestaurantOrderProject.BussinessLayer/Concrete/ProductManager.cs
+++ RestaurantOrderProject.BussinessLayer/Concrete/ProductManager.cs
@@ -12,6 +12,12 @@ namespace RestaurantOrderProject.BussinessLayer.Concrete
     public class ProductManager : IProductService
     {
         private readonly IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
         public void TAdd(Product entity)
         {
             _productDal.Add(entity);
@@ -30,6 +36,11 @@ namespace RestaurantOrderProject.BussinessLayer.Concrete
         public Product TGetById(int id)
         {
             return _productDal.GetById(id);
+        }
+
+        public List<Product> TGetProductsWithCategories()
+        {
+            return _productDal.GetProductsWithCategories();
         }
 
         public void TUpdate(Product entity)
