38d5f21d63a0da822a62389eb8b21a81cdb518bc
seheerst <seherst9@gmail.com>
2023-12-05 15:08:05 +0300

    feat: created controller for admin dashboard

---
+++ RestaurantOrder_UI/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantOrder_UI.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
