47924ceef2ee362b7b2db8daf96c8abda908e9c5
seheerst <seherst9@gmail.com>
2023-12-05 15:10:25 +0300

    feat: admin dashboard divided into component partials

---
+++ RestaurantOrder_UI/ViewComponents/AdminComponents/_LayoutSidebarPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantOrder_UI.ViewComponents.AdminComponents
+{
+	public class _LayoutSidebarPartialComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
