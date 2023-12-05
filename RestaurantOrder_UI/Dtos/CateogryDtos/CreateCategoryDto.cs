4aff5461b03d621caf6a205f164009daa225d698
seheerst <seherst9@gmail.com>
2023-12-05 15:12:03 +0300

    feat: created dtos for category actions

---
+++ RestaurantOrder_UI/Dtos/CateogryDtos/CreateCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RestaurantOrder_UI.Dtos.CateogryDtos
+{
+    public class CreateCategoryDto
+    {
+        public string CategoryName { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ RestaurantOrder_UI/Dtos/CateogryDtos/ResultCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantOrder_UI.Dtos.CateogryDtos
+{
+    public class ResultCategoryDto
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
