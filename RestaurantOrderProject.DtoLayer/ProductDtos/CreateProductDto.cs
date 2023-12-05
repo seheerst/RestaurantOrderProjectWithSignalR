0e6a7dce42b4faecdddddd1dfb72781ec0336480
seheerst <seherst9@gmail.com>
2023-12-05 20:34:40 +0300

    feat: property added for category

--- RestaurantOrderProject.DtoLayer/ProductDtos/CreateProductDto.cs
+++ RestaurantOrderProject.DtoLayer/ProductDtos/CreateProductDto.cs
@@ -13,5 +13,7 @@ namespace RestaurantOrderProject.DtoLayer.ProductDtos
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
         public bool Status { get; set; }
+
+        public int CategoryID { get; set; }
     }
 }
