b4862b37e8fd783f17506c1d9ca86f14cd81c248
seheerst <seherst9@gmail.com>
2023-12-08 20:04:14 +0300

    feat: created dtos for feature actions

---
+++ RestaurantOrder_UI/Dtos/FeatureDtos/UpdateFeatureDto.cs
@@ -0,0 +1,14 @@
+﻿namespace RestaurantOrder_UI.Dtos.FeatureDtos
+{
+    public class UpdateFeatureDto
+    {
+        public int FeatureID { get; set; }
+        public string FeatureName { get; set; }
+        public string Title1 { get; set; }
+        public string Description1 { get; set; }
+        public string Title2 { get; set; }
+        public string Description2 { get; set; }
+        public string Title3 { get; set; }
+        public string Description3 { get; set; }
+    }
+}
