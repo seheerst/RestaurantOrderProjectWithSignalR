903a8c1045e4d15c69ca882d89809eedd167eaac
seheerst <seherst9@gmail.com>
2023-12-09 15:22:43 +0300

    feat: created dtos for social media actions

---
+++ RestaurantOrder_UI/Dtos/SocialMediaDtos/UpdateSocialMediaDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantOrder_UI.Dtos.SocialMediaDtos
+{
+    public class UpdateSocialMediaDto
+    {
+        public int SocilMediaID { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
+    }
+}
