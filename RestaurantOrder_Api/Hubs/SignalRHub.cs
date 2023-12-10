7fa61dcb7b5d1f6119dbe7806d671bcd45527b01
seheerst <seherst9@gmail.com>
2023-12-10 21:06:03 +0300

    feat: created signalR hub class

--- RestaurantOrder_Api/Hubs/SignalRHub.cs
+++ RestaurantOrder_Api/Hubs/SignalRHub.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using RestaurantOrderProject.DataAccessLayer.Concrete;
 
 namespace RestaurantOrder_Api.Hubs
 {
     public class SignalRHub : Hub
     {
+        RestaurantOrderContext context = new RestaurantOrderContext();
+
+        public async Task SendCategoryCount()
+        {
+            var value = context.Categories.Count();
+            await Clients.All.SendAsync("ReceiveCategoryCount", value);
+        }
     }
 }
