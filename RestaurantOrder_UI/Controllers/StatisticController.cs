baa1a6d2586add531cd4ec91408366d161ce9a2a
seheerst <seherst9@gmail.com>
2023-12-12 00:03:33 +0300

    feat: created controller for statistic

---
+++ RestaurantOrder_UI/Controllers/StatisticController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantOrder_UI.Controllers
+{
+    public class StatisticController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
