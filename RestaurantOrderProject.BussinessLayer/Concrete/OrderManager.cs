8b64bde39bc4e31152e686ff7766d0b1db232dd2
seheerst <seherst9@gmail.com>
2023-12-14 22:52:01 +0300

    feat: created order service and manager

---
+++ RestaurantOrderProject.BussinessLayer/Concrete/OrderManager.cs
@@ -0,0 +1,47 @@
+﻿using RestaurantOrderProject.BussinessLayer.Abstract;
+using RestaurantOrderProject.DataAccessLayer.Abstract;
+using RestaurantOrderProject.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOrderProject.BussinessLayer.Concrete
+{
+	public class OrderManager : IOrderService
+	{
+		private readonly IOrderDal _orderDal;
+
+		public OrderManager(IOrderDal orderDal)
+		{
+			_orderDal = orderDal;
+		}
+
+		public void TAdd(Order entity)
+		{
+			_orderDal.Add(entity);
+		}
+
+		public void TDelete(Order entity)
+		{
+			_orderDal.Delete(entity);
+		}
+
+		public List<Order> TGetAllList()
+		{
+			return _orderDal.GetAllList();
+		}
+
+		public Order TGetById(int id)
+		{
+			return _orderDal.GetById(id);
+
+		}
+
+		public void TUpdate(Order entity)
+		{
+			_orderDal.Update(entity);
+		}
+	}
+}
