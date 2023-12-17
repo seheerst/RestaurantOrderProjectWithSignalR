443c4fe4693a671e1ea4802bd41afdc8e052e688
seheerst <seherst9@gmail.com>
2023-12-17 22:31:58 +0300

    feat: add migration for moneyCase entity

--- RestaurantOrderProject.DataLayer/Migrations/20231216165528_mig_add_money_case.Designer.cs
+++ RestaurantOrderProject.DataLayer/Migrations/20231216165528_mig_add_money_case.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestaurantOrderProject.DataAccessLayer.Concrete;
 
@@ -11,9 +12,10 @@ using RestaurantOrderProject.DataAccessLayer.Concrete;
 namespace RestaurantOrderProject.DataAccessLayer.Migrations
 {
     [DbContext(typeof(RestaurantOrderContext))]
-    partial class RestaurantOrderContextModelSnapshot : ModelSnapshot
+    [Migration("20231216165528_mig_add_money_case")]
+    partial class mig_add_money_case
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RestaurantOrderProject.DataLayer/Migrations/20231216165528_mig_add_money_case.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantOrderProject.DataAccessLayer.Migrations
+{
+    public partial class mig_add_money_case : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "TotalPRice",
+                table: "OrderDetails",
+                newName: "TotalPrice");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "Date",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "MoneyCases",
+                columns: table => new
+                {
+                    MoneyCaseID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TotalAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MoneyCases", x => x.MoneyCaseID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MoneyCases");
+
+            migrationBuilder.DropColumn(
+                name: "OrderDate",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "TotalPrice",
+                table: "OrderDetails",
+                newName: "TotalPRice");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
