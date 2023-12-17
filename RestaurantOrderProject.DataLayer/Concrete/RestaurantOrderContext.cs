5a380c305063d47d3d1b61809d3837c05edc2aee
seheerst <seherst9@gmail.com>
2023-12-17 22:32:28 +0300

    feat: add dbSet for moneyCase

--- RestaurantOrderProject.DataLayer/Concrete/RestaurantOrderContext.cs
+++ RestaurantOrderProject.DataLayer/Concrete/RestaurantOrderContext.cs
@@ -27,6 +27,7 @@ namespace RestaurantOrderProject.DataAccessLayer.Concrete
        public DbSet<Testimonial> Testimonials { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<MoneyCase> MoneyCases { get; set; }
 
     }
 }
