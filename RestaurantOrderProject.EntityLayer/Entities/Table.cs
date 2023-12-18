b988b1b938dccdbef62520d556573e1b5ba33625
seheerst <seherst9@gmail.com>
2023-12-18 21:55:50 +0300

    feat: created entity for tables

---
+++ RestaurantOrderProject.EntityLayer/Entities/Table.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOrderProject.EntityLayer.Entities
+{
+	public class Table
+	{
+        public int TableID { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
