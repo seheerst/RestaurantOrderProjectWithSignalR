98d7819eccbfdb63c6706485daae2f4552fe8c81
seheerst <seherst9@gmail.com>
2023-12-19 21:01:38 +0300

    feat: created methods for api consume with signalR

--- RestaurantOrder_Api/Hubs/SignalRHub.cs
+++ RestaurantOrder_Api/Hubs/SignalRHub.cs
@@ -1,16 +1,42 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using RestaurantOrderProject.BussinessLayer.Abstract;
 using RestaurantOrderProject.DataAccessLayer.Concrete;
 
 namespace RestaurantOrder_Api.Hubs
 {
     public class SignalRHub : Hub
     {
-        RestaurantOrderContext context = new RestaurantOrderContext();
+        private readonly ICategoryService _categoryService;
+        private readonly IProductService _productService;
 
-        public async Task SendCategoryCount()
+        public SignalRHub(ICategoryService categoryService, IProductService productService)
+		{
+			_categoryService = categoryService;
+			_productService = productService;
+		}
+
+		public async Task SendStatistics()
         {
-            var value = context.Categories.Count();
-            await Clients.All.SendAsync("ReceiveCategoryCount", value);
-        }
-    }
+            var value1 = _categoryService.TGetCategoryCount();
+            await Clients.All.SendAsync("ReceiveCategoryCount", value1);
+
+			var value2 = _productService.TGetProductCount();
+			await Clients.All.SendAsync("ReceiveProductCount", value2);
+
+			var value3 = _categoryService.TActiveCategoryCount();
+			await Clients.All.SendAsync("ReceiveActiveCategoryCount", value3);
+
+			var value4 = _categoryService.TPassiveCategoryCount();
+			await Clients.All.SendAsync("ReceivePassiveCategoryCount", value4);
+
+			var value5 = _productService.TProductCountByCategoryNameHamburger();
+			await Clients.All.SendAsync("ReceiveProductCountByHamburger", value5);
+
+			var value6 = _productService.TProductCountByCategoryNameDrink();
+			await Clients.All.SendAsync("ReceiveProductCountByDrink", value6);
+
+		}
+
+
+	}
 }
