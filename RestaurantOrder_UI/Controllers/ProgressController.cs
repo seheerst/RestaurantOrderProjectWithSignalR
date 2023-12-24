53481893cedc9a513feaafc5c8c5a09c402f8dcc
seheerst <seherst9@gmail.com>
2023-12-24 22:22:13 +0300

    feat: created controller for progress bar

---
+++ RestaurantOrder_UI/Controllers/ProgressController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantOrder_UI.Controllers
+{
+    public class ProgressController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
