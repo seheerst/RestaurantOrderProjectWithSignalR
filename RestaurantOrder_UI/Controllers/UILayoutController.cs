626eac4c958eb09aeebbcd89f2d4f5230b2a56f0
seheerst <seherst9@gmail.com>
2024-01-07 20:55:33 +0300

    feat: controller for UI layout

---
+++ RestaurantOrder_UI/Controllers/UILayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantOrder_UI.Controllers
+{
+    public class UILayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
