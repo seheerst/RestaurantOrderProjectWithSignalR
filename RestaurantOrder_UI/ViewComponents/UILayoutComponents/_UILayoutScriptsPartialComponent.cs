fa7d2d183e8a1c0e6bc3fdf6bddc4a3e49389a9e
seheerst <seherst9@gmail.com>
2024-01-07 20:57:29 +0300

    feat: created components for UI layout partials

---
+++ RestaurantOrder_UI/ViewComponents/UILayoutComponents/_UILayoutScriptsPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantOrder_UI.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutScriptsPartialComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
