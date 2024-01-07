af68f93144fc894e1984cca3f1d8535461eb696f
seheerst <seherst9@gmail.com>
2024-01-07 20:56:02 +0300

    feat: created controller for default home page

---
+++ RestaurantOrder_UI/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantOrder_UI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
