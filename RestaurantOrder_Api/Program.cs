de35ef647b8fa6c2cefceca3f9770393cd334a78
seheerst <seherst9@gmail.com>
2024-01-23 19:29:22 +0300

    created api controller for slider

--- RestaurantOrder_Api/Program.cs
+++ RestaurantOrder_Api/Program.cs
@@ -71,6 +71,9 @@ builder.Services.AddScoped<IMoneyCaseDal, EfMoneyCaseDal>();
 builder.Services.AddScoped<ITableService, TableManager>();
 builder.Services.AddScoped<ITableDal, EfTableDal>();
 
+builder.Services.AddScoped<ISliderService, SliderManager>();
+builder.Services.AddScoped<ISliderDal, EfSliderDal>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
