c9c5e953f7c6ccb95ba76dd088adc98128ccb751
seheerst <seherst9@gmail.com>
2024-01-23 19:29:01 +0300

    created partials for default home page

--- RestaurantOrder_UI/ViewComponents/DefaultComponents/_DefaultMenuPartialComponent.cs
+++ RestaurantOrder_UI/ViewComponents/DefaultComponents/_DefaultMenuPartialComponent.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestaurantOrder_UI.Dtos.ProductDtos;
 
 namespace RestaurantOrder_UI.ViewComponents.DefaultComponents
 {
     public class _DefaultMenuPartialComponent: ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultMenuPartialComponent(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7050/api/Product");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
--- RestaurantOrder_UI/ViewComponents/DefaultComponents/_DefaultSliderPartialComponent.cs
+++ RestaurantOrder_UI/ViewComponents/DefaultComponents/_DefaultSliderPartialComponent.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestaurantOrder_UI.Dtos.SliderDtos;
 
 namespace RestaurantOrder_UI.ViewComponents.DefaultComponents
 {
     public class _DefaultSliderPartialComponent : ViewComponent
     {
-        public IViewComponentResult Invoke() 
+         
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultSliderPartialComponent(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+    
+        public async Task<IViewComponentResult> InvokeAsync() 
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7050/api/Slider");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSliderDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
