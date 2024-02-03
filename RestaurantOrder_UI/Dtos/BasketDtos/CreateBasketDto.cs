b66903e5e07e451fd288b884bda213a94f00ec2a
seheerst <seherst9@gmail.com>
2024-02-03 20:15:54 +0300

    created dtos for basket class

---
+++ RestaurantOrder_UI/Dtos/BasketDtos/CreateBasketDto.cs
@@ -0,0 +1,15 @@
+﻿using RestaurantOrderProject.EntityLayer.Entities;
+
+namespace RestaurantOrder_UI.Dtos.BasketDtos
+{
+    public class CreateBasketDto
+    {
+        public int ProductID { get; set; }
+        public Product Product { get; set; }
+        public decimal Price { get; set; }
+        public decimal Count { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int TableID { get; set; }
+        public Table Table { get; set; }
+    }
+}
---
+++ RestaurantOrder_UI/Dtos/BasketDtos/GetBasketDto.cs
@@ -0,0 +1,16 @@
+﻿using RestaurantOrderProject.EntityLayer.Entities;
+
+namespace RestaurantOrder_UI.Dtos.BasketDtos
+{
+    public class GetBasketDto
+    {
+        public int BasketID { get; set; }
+        public int ProductID { get; set; }
+        public Product Product { get; set; }
+        public decimal Price { get; set; }
+        public decimal Count { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int TableID { get; set; }
+        public Table Table { get; set; }
+    }
+}
