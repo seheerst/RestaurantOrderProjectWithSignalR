403547fc1c798bc019790416d27d2469d254ce46
seheerst <seherst9@gmail.com>
2024-02-09 18:02:20 +0300

    added addBasket method

--- RestaurantOrder_UI/Controllers/MenuController.cs
+++ RestaurantOrder_UI/Controllers/MenuController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using RestaurantOrder_UI.Dtos.BasketDtos;
 using RestaurantOrder_UI.Dtos.ProductDtos;
+using System.Text;
 
 namespace RestaurantOrder_UI.Controllers
 {
@@ -25,5 +27,22 @@ namespace RestaurantOrder_UI.Controllers
             }
             return View();
         }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> AddBasket(int id)
+        {
+            CreateBasketDto createBasketDto = new CreateBasketDto();
+            createBasketDto.ProductID = id;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBasketDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7050/api/Basket", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return Json(createBasketDto);
+        }
     }
 }
+
