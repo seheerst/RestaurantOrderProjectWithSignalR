c49cde8edf23f154a9e66dfec79a80953343ffd0
seheerst <seherst9@gmail.com>
2024-02-14 23:42:26 +0300

    added notification service and implamentation

---
+++ RestaurantOrderProject.BussinessLayer/Abstract/INotificationService.cs
@@ -0,0 +1,16 @@
+﻿using RestaurantOrderProject.DataAccessLayer.Abstract;
+using RestaurantOrderProject.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOrderProject.BussinessLayer.Abstract
+{
+    public interface INotificationService : IGenericService<Notification>
+
+    {
+        int TNotificationCountByStatusFalse();
+    }
+}
