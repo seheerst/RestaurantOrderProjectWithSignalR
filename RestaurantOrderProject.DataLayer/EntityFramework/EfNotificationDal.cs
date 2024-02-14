db243a5e08d7891cb400f77fa5121b0e4082f1a9
seheerst <seherst9@gmail.com>
2024-02-14 23:39:32 +0300

    created notification dal interface and implamentation

---
+++ RestaurantOrderProject.DataLayer/EntityFramework/EfNotificationDal.cs
@@ -0,0 +1,27 @@
+﻿using RestaurantOrderProject.DataAccessLayer.Abstract;
+using RestaurantOrderProject.DataAccessLayer.Concrete;
+using RestaurantOrderProject.DataAccessLayer.Repositories;
+using RestaurantOrderProject.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOrderProject.DataAccessLayer.EntityFramework
+{
+    public class EfNotificationDal : GenericRepository<Notification>, INotificationDal
+    {
+        private readonly RestaurantOrderContext _context;
+        public EfNotificationDal(RestaurantOrderContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public int NotificationCountByStatusFalse()
+        {
+            var count = _context.Notifications.Where(x=> x.Status == false).Count();
+            return count;
+        }
+    }
+}
