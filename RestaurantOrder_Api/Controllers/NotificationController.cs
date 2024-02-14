62b953bf911bf37536ba1631926c41aa37111b48
seheerst <seherst9@gmail.com>
2024-02-14 23:42:54 +0300

    created api controller for notification

---
+++ RestaurantOrder_Api/Controllers/NotificationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantOrderProject.BussinessLayer.Abstract;
+
+namespace RestaurantOrder_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+        [HttpGet]
+        public IActionResult NotificationList() {
+            var values = _notificationService.TGetAllList();
+            return Ok(values);
+        }
+
+        [HttpGet("NotificationCountByStatusFalse")]
+
+        public IActionResult NotificationCountByStatusFalse()
+        {
+            var count = _notificationService.TNotificationCountByStatusFalse();
+            return Ok(count);
+        }
+    }
+}
