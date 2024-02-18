4a80ae5fd4ae1c4d3ff09466a20c5163deb3673c
seheerst <seherst9@gmail.com>
2024-02-18 16:36:55 +0300

    deleted empty prop

--- RestaurantOrderProject.EntityLayer/Entities/Notification.cs
+++ RestaurantOrderProject.EntityLayer/Entities/Notification.cs
@@ -14,7 +14,6 @@ namespace RestaurantOrderProject.EntityLayer.Entities
         public string Icon { get; set; }
         public DateTime Date { get; set; }
         public bool Status { get; set; }
-        public int MyProperty { get; set; }
 
     }
 }
