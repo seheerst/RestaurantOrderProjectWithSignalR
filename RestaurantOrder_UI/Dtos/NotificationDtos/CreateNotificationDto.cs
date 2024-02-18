397766ef8f121fdde89a376e5d5ad055cc9d9d43
seheerst <seherst9@gmail.com>
2024-02-18 16:40:55 +0300

    created dtos for notification

---
+++ RestaurantOrder_UI/Dtos/NotificationDtos/CreateNotificationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantOrder_UI.Dtos.NotificationDtos
+{
+    public class CreateNotificationDto
+    {
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string Icon { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+    }
+}
