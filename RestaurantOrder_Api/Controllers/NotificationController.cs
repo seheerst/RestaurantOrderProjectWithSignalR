710f787a59307d24c054139e39d183c1fa2a23c1
seheerst <seherst9@gmail.com>
2024-02-18 16:38:49 +0300

    added methods for get, delete and update

--- RestaurantOrder_Api/Controllers/NotificationController.cs
+++ RestaurantOrder_Api/Controllers/NotificationController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RestaurantOrderProject.BussinessLayer.Abstract;
+using RestaurantOrderProject.DtoLayer.NotificationDtos;
+using RestaurantOrderProject.EntityLayer.Entities;
 
 namespace RestaurantOrder_Api.Controllers
 {
@@ -26,6 +28,56 @@ namespace RestaurantOrder_Api.Controllers
         {
             var count = _notificationService.TNotificationCountByStatusFalse();
             return Ok(count);
+        }
+
+        [HttpGet("GetAllNotificationByStatusFalse")]
+        public IActionResult GetAllNotificationByStatusFalse()
+        {
+            var values = _notificationService.TGetAllNotificationByStatusFalse();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateNotification(CreateNotificationDto createNotificationDto)
+        {
+            Notification notification = new Notification()
+            {
+                Description = createNotificationDto.Description,
+                Type = createNotificationDto.Type,
+                Icon = createNotificationDto.Icon,
+                Date = createNotificationDto.Date,
+                Status = createNotificationDto.Status
+            };
+            _notificationService.TAdd(notification);
+            return Ok("Eklendi");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteNotification(int id)
+        {
+            var value = _notificationService.TGetById(id);
+            _notificationService.TDelete(value);
+            return Ok("Silindi");
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateNotification(UpdateNotificationDto updateNotificationDto)
+        {
+            Notification notification = new Notification()
+            {
+                NotificationID = updateNotificationDto.NotificationID,
+                Description = updateNotificationDto.Description,
+                Type = updateNotificationDto.Type,
+                Icon = updateNotificationDto.Icon,
+                Date = updateNotificationDto.Date,
+                Status = updateNotificationDto.Status
+            };
+            _notificationService.TUpdate(notification);
+            return Ok("Güncellendi");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetNotification(int id)
+        {
+            var value = _notificationService.TGetById(id);
+            return Ok(value);
         }
     }
 }
