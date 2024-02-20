f84935cae892df3c753e956e2ee0295d2e5987df
seheerst <seherst9@gmail.com>
2024-02-20 21:11:01 +0300

    added actions for crud

--- RestaurantOrder_Api/Controllers/TableController.cs
+++ RestaurantOrder_Api/Controllers/TableController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RestaurantOrderProject.BussinessLayer.Abstract;
+using RestaurantOrderProject.DtoLayer.TableDtos;
+using RestaurantOrderProject.EntityLayer.Entities;
 
 namespace RestaurantOrder_Api.Controllers
 {
@@ -20,5 +22,54 @@ namespace RestaurantOrder_Api.Controllers
 		{
 			return Ok(_tableService.TMenuTableCount());
 		}
-	}
+
+        [HttpGet]
+        public IActionResult TableList()
+        {
+            var values = _tableService.TGetAllList();
+            return Ok(values);
+        }
+        [HttpPost]
+
+        public IActionResult CreateTable(CreateTableDto createTableDto)
+        {
+            Table table = new Table()
+            {
+                Name = createTableDto.Name,
+                Status = false,
+            };
+             _tableService.TAdd(table);
+            return Ok("Eklendi");
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTable(int id)
+        {
+            var value = _tableService.TGetById(id);
+            _tableService.TDelete(value);
+            return Ok("Silindi");
+        }
+        [HttpPut]
+        public IActionResult UpdateTable(UpdateTableDto updateTableDto)
+        {
+            Table table = new Table()
+            {
+                TableID = updateTableDto.TableID,
+                Name = updateTableDto.Name,
+                Status = false,
+                
+            };
+            _tableService.TUpdate(table);
+            return Ok("Güncellendi");
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetTable(int id)
+        {
+            var value = _tableService.TGetById(id);
+            return Ok(value);
+        }
+    }
 }
