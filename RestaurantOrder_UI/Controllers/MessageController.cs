4592c30816eeb9d21d9c1752fe35718b62ec2b9c
seheerst <seherst9@gmail.com>
2024-02-21 19:47:08 +0300

    created controller for message

---
+++ RestaurantOrder_UI/Controllers/MessageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantOrder_UI.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ClientUserCount()
+        {
+            return View();
+        }
+    }
+}
