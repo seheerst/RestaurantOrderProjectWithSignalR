422824cef20c98c40c8b41efcd9b0e98dd0032a1
seheerst <seherst9@gmail.com>
2024-03-04 19:56:00 +0300

    created controller for login and register

---
+++ RestaurantOrder_UI/Controllers/RegisterController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantOrder_UI.Dtos.IdentityDtos;
+using RestaurantOrderProject.EntityLayer.Entities;
+
+namespace RestaurantOrder_UI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(RegisterDto registerDto)
+        {
+            AppUser appUser = new AppUser()
+            {
+                Name = registerDto.Name,
+                Surname = registerDto.Surname,
+                Email = registerDto.Mail,
+                UserName = registerDto.UserName,
+
+            };
+            var result = await _userManager.CreateAsync(appUser, registerDto.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+
+    }
+}
